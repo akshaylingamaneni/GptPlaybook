dfac7ab1c17aa4cd57f2ee8b95fdce61ba8674fc
akshaylingamaneni <lingamaneniakshay@gmail.com>
2024-02-20 22:29:14 -0600

    playbook setup

---
+++ src/test/java/net/if_ai/gptplaybook/GptPlaybookApplicationTests.java
@@ -0,0 +1,13 @@
+package net.if_ai.gptplaybook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GptPlaybookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
